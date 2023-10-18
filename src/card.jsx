6b40919f29a0bf8b719fd6789ceab110486c6561
Harsh Rai <harshpayal3252@gmail.com>
2023-10-18 13:31:15 +0530

    Add node_modules to .gitignore

---
+++ src/card.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Card = ({ name, photo, handleClick }) => {
+  return (
+    <div className="card" onClick={() => handleClick(photo,name)}>
+      <img src={photo} alt={name} />
+      <p>{name}</p>
+    </div>
+  );
+};
+
+export default Card;
